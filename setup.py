3103a24cb9d255e6d9ba3019e69b2db9deec6910
maxwesemeyer <maxwesemeyer@gmail.com>
2020-05-20 14:40:11 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='wesobia',
+setup(name='Wes_Tools',
       version='0.1',
       description='python package containing small utility functions for Object based image analysis',
-      url='https://github.com/maxwesemeyer/wesobia',
+      url='https://github.com/maxwesemeyer/Wes_Tools/',
       author='Maximilian Wesemeyer',
       author_email='maximilian.wesemeyer@hu-berlin.de',
       license='MIT',
-      packages=['wesobia'],
+      packages=['Wes_Tools'],
       zip_safe=False)
