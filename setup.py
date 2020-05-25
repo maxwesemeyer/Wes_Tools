dddebcaa29490ba7407b3cdc352809daeaaa8d8f
maxwesemeyer <maxwesemeyer@gmail.com>
2020-05-25 13:46:29 +0200

    updates

--- setup.py
+++ setup.py
@@ -8,4 +8,7 @@ setup(name='Wes_Tools',
       author_email='maximilian.wesemeyer@hu-berlin.de',
       license='MIT',
       packages=['Wes_Tools'],
+      install_requires=['numpy', 'pytorch', 'matplotlib', 'rasterio', 'scikit-image', 'scikit-learn',
+                        'imageio', 'shapely', 'scipy', 'dictances', 'pandas', 'shapely',
+                        'joblib', 'affine'],
       zip_safe=False)
