f8d7dca4c5d72e7b9ecd4a0861b457db9ad0d6d9
maxwesemeyer <maxwesemeyer@gmail.com>
2020-05-28 11:10:53 +0200

    updates

--- Scripts/__Validation_Script.py
+++ Scripts/__Validation_Script.py
@@ -22,46 +22,23 @@ if __name__ == '__main__':
     raster_path = 'X:/SattGruen/Analyse/GLSEG/Raster/X0068_Y0042/2018-2018_001-365_LEVEL4_TSA_SEN2L_NDV_TSS.tif'
     vector_path = data_path + 'Vector/dissolved_paulinaue_3035_parcels.gpkg'
 
-    list_of_shapes = Shape_finder('W:/Student_Data/Wesemeyer/Master/results_new/')
+    list_of_shapes = Shape_finder(data_path + 'joined/')
     print(list_of_shapes)
     pse_list = []
     overall_list = []
+    clinton_list = []
 
     for shapes in list_of_shapes:
 
         try:
-            gdf = gpd.GeoDataFrame(pd.concat([gpd.read_file(list_of_shapes[23])], ignore_index=True),
-                                   crs=gpd.read_file(list_of_shapes[23]).crs)
 
-            # drop cluster number 0, which is all no grassland polygons
-            indexNames = gdf[gdf['Cluster_nb'] == 0].index
-            gdf.drop(indexNames, inplace=True)
-
-            # drop all entries with field nb = na, which don't have a geometry and are duplicates
-            indexNames_2 = gdf[np.isnan(gdf['field_nb'])].index
-            gdf.drop(indexNames_2, inplace=True)
-
-            x = Parallel(n_jobs=1)(
-                delayed(aggregator)(
-                    raster_NDV='X:/lower_saxony_sentinel2_TSA_coreg/X0061_Y0046/2018-2020_001-365_HL_TSA_SEN2L_NDV_TSS.tif',
-                    shapei=row, indexo=index, subsetter=None) for
-                index, row in gdf.iterrows())
-            mergo = pd.DataFrame(x)
-            print('Mergo:', mergo)
-        except:
-            print('error')
-            continue
-
-"""
-        try:
-            DistancoTron('W:/Student_Data/Wesemeyer/Projekt_/Shapes/RH_UG_3035.shp',
-                         'X:/lower_saxony_sentinel2_TSA_coreg/X0061_Y0046/2018-2020_001-365_HL_TSA_SEN2L_NDV_TSS.tif')
-            pse, nsr, ed2 = Accuracy_Assessment.Liu('W:/Student_Data/Wesemeyer/Projekt_/Shapes/RH_UG_3035.shp', shapes)
-            #print(np.mean(np.array(ed2)))
-            US, OS, Overall = Accuracy_Assessment.Clinton('W:/Student_Data/Wesemeyer/Projekt_/Shapes/RH_UG_3035.shp', shapes)
-            #print(np.mean(np.array(Overall)))
-            pse_list.append(np.mean(np.array(ed2)))
-            overall_list.append(np.mean(np.array(Overall)))
+            pse, nsr, ed2 = Accuracy_Assessment.Liu(data_path + 'Vector/Paulienenaue_TF.shp', shapes)
+            print(np.mean(np.array(ed2)))
+            US, OS, Overall = Accuracy_Assessment.Clinton(data_path + 'Vector/Paulienenaue_TF.shp', shapes)
+            print(np.mean(np.array(Overall)))
+            overall_list.append(np.mean(np.array(ed2)))
+            pse_list.append(np.mean(np.array(pse)))
+            clinton_list.append(np.mean(np.array(Overall)))
         except:
             pse_list.append(10)
             overall_list.append(10)
@@ -70,5 +47,5 @@ if __name__ == '__main__':
     print(np.argmin(np.array(pse_list)))
     print(np.argmin(np.array(overall_list)))
     print(np.array(pse_list), np.argmin(np.array(pse_list)), np.argmin(np.array(overall_list)))
-    print('According to PSE (ED2)', list_of_shapes[np.argmin(np.array(pse_list))], 'with a score of ', np.min(np.array(pse_list)))
-    print('According to Clinton', list_of_shapes[np.argmin(np.array(overall_list))], 'with a score of ', np.min(np.array(overall_list)))
+    print('According to PSE (ED2)', list_of_shapes[np.argmin(np.array(overall_list))], 'with a score of ', np.min(np.array(overall_list)))
+    print('According to Clinton', list_of_shapes[np.argmin(np.array(clinton_list))], 'with a score of ', np.min(np.array(clinton_list)))
